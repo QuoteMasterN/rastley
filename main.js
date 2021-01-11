3c556454cda837de5bd0982460fce84de8027fe3
Nathan Smith <nathanrs1002@gmail.com>
2021-01-10 18:35:46 -0600

    Initial main.js Contains code to get token from user and login into bot account.

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-const Discord = require('discord.js');
+// Library constants
+const discord = require('discord.js');
+const axios = require('axios').default;
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// discord client for the bot
+const client = new discord.Client();
+
+// bot token
+var token;
+
+// get bot token
+readline.question('Enter bot token:', input => {
+    token = input;
+    readline.close();
+    // login to discord
+    client.login(token);
+});
+
+client.once('ready', () => {
+    console.log('Rastley is online!');
+});
+
